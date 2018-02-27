056be05edd41770fda1786a42086400b2ddbf383
DeAndre Jones <drej08@yahoo.com>
2018-02-26 21:42:57 -0600

    Add view did disappear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -23,5 +23,9 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
 //        <#code#>
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        //
+    }
 }
 
